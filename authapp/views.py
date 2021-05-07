96a26e16b8b88dd8f7a0d90996dcc21519b9a735
Nail <Angel2326@yandex.ru>
2021-05-08 01:06:46 +0300

    подготовлена форма для регистрации пользователя

--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+def login(request):
+    context = {'title' :'GeekShop - Авторизация'}
+    return render(request,'authapp/login.html', context)
 # Create your views here.
