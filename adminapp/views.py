fa40ca21dcb48e0205918f7d438d9b8f162db050
Nail <Angel2326@yandex.ru>
2021-05-21 21:37:37 +0300

    panel admin-users-read is done

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from authapp.models import New_user
+
 
 # Create your views here.
 
 def index(request):
     return render(request, 'adminapp/admin.html')
+
+
+def admin_users_read(request):
+    context = {'users':New_user.objects.all()}
+    return render(request, 'adminapp/admin-users-read.html', context)
