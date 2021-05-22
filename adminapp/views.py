4691b716701dc93fb86bccb549dc8f279bee0e1b
Nail <Angel2326@yandex.ru>
2021-05-22 18:29:27 +0300

    panel admin-users-create is done

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 from authapp.models import New_user
-
+from adminapp.forms import UserAdminRegisterForm, UserRegisterForm
+from django.contrib import messages
 
 # Create your views here.
 
 def index(request):
+
     return render(request, 'adminapp/admin.html')
 
 
 def admin_users_read(request):
     context = {'users':New_user.objects.all()}
     return render(request, 'adminapp/admin-users-read.html', context)
+
+def admin_users_create(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Пользователь успешно создан')
+            return HttpResponseRedirect(reverse('admin_staff:admin_users_read'))
+        else:
+            print(form.errors)
+    form = UserAdminRegisterForm()
+    context = {'form': form}
+    return render(request, 'adminapp/admin-users-create.html', context)
